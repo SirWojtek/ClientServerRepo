af2e458f06253943d6d33a7de9309f7d9e6b3904
SirWojtek <momatoku@gmail.com>
2014-08-16 11:07:32 +0200

    Added boost libraries

--- client/client/main.cpp
+++ client/client/main.cpp
@@ -1,5 +1,12 @@
+#include <iostream>
+#include <boost\asio\write.hpp>
 
 int main(int argc, char** argv)
 {
+	std::cout << "Hello World!" << std::endl;
+
+	std::shared_ptr<int> p1 = std::make_shared<int>(10);
+
+	system("pause");
 	return 0;
 }
