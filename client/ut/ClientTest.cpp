2000f2ed0c09eb0ef74e1d4a75a953a615636936
SirWojtek <momatoku@gmail.com>
2015-02-24 22:52:44 +0100

    Added client ut covers new implementation

--- client/ut/ClientTest.cpp
+++ client/ut/ClientTest.cpp
@@ -39,6 +39,24 @@ TEST_F(ClientShould, ExitWhenExitKeyWasPressed)
     EXPECT_EQ(0, client_->start(0, nullptr));
 }
 
+TEST_F(ClientShould, GetKeyboardInputWhenNoExitKeyWasPressed)
+{
+    const unsigned loops = 5;
+
+    Expectation inputExpect =
+    EXPECT_CALL(*keyboardControllerMock_, wasExitKeyPressed())
+        .Times(loops)
+        .WillRepeatedly(Return(false));
+    EXPECT_CALL(*keyboardControllerMock_, getKeyboardInput())
+        .Times(loops)
+        .WillRepeatedly(Return(KeyboardControllerMock::None));
+    EXPECT_CALL(*keyboardControllerMock_, wasExitKeyPressed())
+        .After(inputExpect)
+        .WillOnce(Return(true));
+
+    EXPECT_EQ(0, client_->start(0, nullptr));
+}
+
 TEST_F(ClientShould, catchThrowedException)
 {
     EXPECT_CALL(*keyboardControllerMock_, wasExitKeyPressed())
