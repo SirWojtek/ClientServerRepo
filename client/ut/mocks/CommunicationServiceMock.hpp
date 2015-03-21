77fc54903e4c3486fc4c9c90d2bc0c9f99b3a7d8
SirWojtek <momatoku@gmail.com>
2015-03-21 16:55:13 +0100

    Fixed compilation of client uts

--- client/ut/mocks/CommunicationServiceMock.hpp
+++ client/ut/mocks/CommunicationServiceMock.hpp
@@ -2,18 +2,15 @@
 #define COMMUNICATION_SERVICE_MOCK_HPP_
 
 #include <gmock/gmock.h>
-#include <memory>
 
 #include "client/src/ICommunicationService.hpp"
-#include "common/messages/OkResponse.hpp"
-#include "common/messages/UpdatePlayer.hpp"
-#include "common/messages/MessageUtilities.hpp"
 
 class CommunicationServiceMock : public ICommunicationService
 {
 public:
     MOCK_METHOD0(startService, void());
     MOCK_METHOD1(putMessageInQueue, common::OkResponse(const common::UpdatePlayer& message));
+    MOCK_METHOD1(putMessageInQueue, common::OkResponse(const common::Login& message));
     MOCK_METHOD2(getMessage, std::shared_ptr<std::string>(
         const common::messagetype::MessageType& type, bool wait));
     MOCK_METHOD0(tearDown, void());
