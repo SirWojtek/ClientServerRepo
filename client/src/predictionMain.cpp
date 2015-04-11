1b5c43b96cf75eb3353cc9e70572969e11bab646
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-04-11 17:57:02 +0200

    Added couple of test scenario and improved prints

--- client/src/predictionMain.cpp
+++ client/src/predictionMain.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <algorithm>
 
-prediction::FileVector getFiles(int argc, char** argv)
+using namespace prediction;
+
+FileVector getFiles(int argc, char** argv)
 {
     prediction::FileVector files;
 
@@ -17,17 +19,9 @@ prediction::FileVector getFiles(int argc, char** argv)
     return files;
 }
 
-int main(int argc, char** argv)
+void runTest(const FileVector& files, PredictionAssistant::PredictionFunction func)
 {
-    using namespace prediction;
-
-    if (argc < 2)
-    {
-        return -1;
-    }
-
-    FileVector files = getFiles(argc, argv);
-    PredictionAssistant assistant(3, 3, &DistanceFunctions::startPointsDistance);
+    PredictionAssistant assistant(3, 3, func);
 
     for (unsigned i = 0; i < files.size() - 1; i++)
     {
@@ -41,6 +35,23 @@ int main(int argc, char** argv)
     int passed = std::count_if(results.begin(), results.end(), [](bool a){ return a; });
     float floatPassed = static_cast<float>(passed)/static_cast<float>(results.size());
     std::cout << "Percent prediction match: " << floatPassed * 100 << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        return -1;
+    }
+
+    FileVector files = getFiles(argc, argv);
+
+    std::cout << "**** Testing with start points distance ***" << std::endl;
+    runTest(files, &DistanceFunctions::startPointsDistance);
+    std::cout << std::endl << "**** Testing with direction distance ***" << std::endl;
+    runTest(files, &DistanceFunctions::directionDistance);
+    std::cout << std::endl << "**** Testing with start points and direction distance ***" << std::endl;
+    runTest(files, &DistanceFunctions::startPointsDistanceAndDirection);
 
     return 0;
 }
