3b28b60c4d07f5339ed96313fe8129860e9475e0
SirWojtek <momatoku@gmail.com>
2015-04-13 21:04:58 +0200

    Added test for distance functions

--- client/ut/main.cpp
+++ client/ut/main.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char** argv)
 {
-	ILoger::offPrint_ = true;
-	::testing::InitGoogleMock(&argc, argv);
-	::testing::InitGoogleTest(&argc, argv);
+    ILoger::offPrint_ = true;
+    ::testing::InitGoogleMock(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
 
-	return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
