91a00774b53afcd262abeb802884b3c59119bb88
SirWojtek <momatoku@gmail.com>
2015-04-13 21:22:58 +0200

    Added direction choose function tests

--- client/src/prediction/DirectionChooseFunctions.hpp
+++ client/src/prediction/DirectionChooseFunctions.hpp
@@ -6,14 +6,11 @@
 
 namespace prediction
 {
-struct DeltaRecord;
 
 DeltaRecord getClosestNeighborDirectionWithMaxOccurence(
     const BasicKNeighborFinder::NeighborsVector& neighbors);
 DeltaRecord getDirectionWithMaxOccurence(
     const BasicKNeighborFinder::NeighborsVector& neighbors);
-// DeltaRecord getDirectionWithMaxOccurenceFromWeightedNeighbors(
-//     const BasicKNeighborFinder::NeighborsVector& neighbors);
 
 }
 
