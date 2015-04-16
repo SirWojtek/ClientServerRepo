1f66fc9a1d173a45d0a7eb24035210dd9d859c56
SirWojtek <momatoku@gmail.com>
2015-04-16 18:03:51 +0200

    Adjust records number

--- client/src/predictionMain.cpp
+++ client/src/predictionMain.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
 
     FileVector files = getFiles(argc, argv);
     DistanceFunctions::setWeight(0.01);
-    std::vector<unsigned> recordsNumber = { 10, 20, 50, 100, 200, 500, 1000 };
+    std::vector<unsigned> recordsNumber = { 5, 10, 20, 50, 100, 200, 500, 1000 };
 
     for (unsigned number : recordsNumber)
     {
